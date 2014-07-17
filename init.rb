6dd0bf1d7059f700add82e6ad3b5f52c7581e151
Piotr Prusinski <piotr.prusinski@agora.pl>
2014-07-17 14:09:08 +0300

    Init redmine plugin after run script ruby script/rails generate redmine_plugin redmine_log_time_control

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_log_time_control do
+  name 'Redmine Log Time Control plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
